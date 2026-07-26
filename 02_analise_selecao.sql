ef922bd39fc28a8cdf7ba66d71e733f98cf93ecb
samuel <samuel@reppublica.com.br>
2026-07-26 15:03:29 -0300

    02 - Análise das Seleções

--- 02_analise_selecao.sql
+++ 02_analise_selecao.sql
@@ -16,11 +16,73 @@ Qual seleção marcou mais gols?
 
 */
 
-SELECT t2.team_name,
-        SUM(goals) as totalGols
-FROM player_stats as t1
+-- SELECT t2.team_name,
+--         SUM(t1.goals) as totalGols
+-- FROM player_stats as t1
 
-LEFT JOIN teams as t2 
+-- LEFT JOIN teams as t2 
+-- ON t1.team_id = t2.team_id
+-- GROUP BY t1.team_id
+-- ORDER BY totalGols DESC 
+
+
+/*
+----------------------------------------------------
+Pergunta 2
+
+Qual sofreu menos gols?
+
+*/
+
+-- SELECT t2.team_name,
+--         SUM(CAST(t1.goals_conceded AS INT)) AS golsSofridos
+-- FROM player_stats as t1
+
+-- LEFT JOIN teams as t2 
+-- ON t1.team_id = t2.team_id
+-- GROUP BY t1.team_id
+-- ORDER BY golsSofridos DESC 
+
+/*
+----------------------------------------------------
+Pergunta 3
+
+Qual teve o maior saldo de gols?
+
+*/
+
+-- WITH estatisticas as (
+-- SELECT  t2.team_name,
+--         SUM(goals) as totalGols, 
+--         SUM(CAST(goals_conceded AS INT)) AS golsSofridos
+        
+-- FROM player_stats as t1
+-- LEFT JOIN teams as t2
+-- ON t1.team_id = t2.team_id
+-- GROUP BY t1.team_id
+-- )
+
+
+-- SELECT *,
+--         totalGols - golsSofridos AS saldo
+-- FROM estatisticas
+-- ORDER BY saldo DESC
+
+/*
+----------------------------------------------------
+Pergunta 4
+
+Qual teve maior posse de bola média?
+
+*/
+
+SELECT t1.team_id,
+        t2.team_name,
+        ROUND(AVG(possession_pct), 2) AS media_posse,
+        COUNT(*) AS total_partidas
+
+FROM match_team_stats as t1
+LEFT JOIN teams as t2
 ON t1.team_id = t2.team_id
-GROUP BY t1.team_id
-ORDER BY totalGols DESC 
+GROUP BY t1.team_id 
+ORDER BY media_posse DESC
